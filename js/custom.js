b62f6519f489b2213b86fa3a4c59de0aacc0825c
Akhil Chandran <144078881+akhiirulam@users.noreply.github.com>
2023-09-14 20:22:22 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -126,7 +126,7 @@
     }
        
     
-   /* // Prevent the form from being submitted if there are any errors
+   // Prevent the form from being submitted if there are any errors
     if((cfname || cfemail || cfcontact) == true) {
        return false;
     } else {
@@ -137,6 +137,6 @@
                           "Mobile Number: " + mobile+ "\n";
         
         // Display input data in a dialog box before submitting the form
-        alert(dataPreview);
-    }*/
+       /* alert(dataPreview);*/
+    }
 };
